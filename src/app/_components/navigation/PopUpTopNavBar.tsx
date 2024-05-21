ddb1884b19e38da1248d6de10593453cf4519745
Wanjin Noh <wanzargen@gmail.com>
2024-05-21 10:02:05 +0900

    feat: remove onDiscard

--- src/app/_components/navigation/PopUpTopNavBar.tsx
+++ src/app/_components/navigation/PopUpTopNavBar.tsx
@@ -4,21 +4,14 @@ import { useRouter } from 'next/navigation';
 
 interface Props {
   title: string;
-  onDiscard?: () => void;
 }
 
-export default function PopUpTopNavBar({ title, onDiscard }: Props) {
+export default function PopUpTopNavBar({ title }: Props) {
   const router = useRouter();
-  const handleClickDiscard = () => {
-    if (onDiscard) {
-      onDiscard();
-    }
-    router.back()
-  }
   return (
     <div className="pop-up-top-nav-bar">
       <h2>{title}</h2>
-    <button type="button" onClick={handleClickDiscard}>
+    <button type="button" onClick={() => router.back()}>
       Discard
     </button>
     </div>
